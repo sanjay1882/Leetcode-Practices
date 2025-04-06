457235b2baad95b4c8d64185eef65a1aa31183b2
Sanjayraju <132285559+sanjay1882@users.noreply.github.com>
2025-04-06 12:05:19 +0530

    Time: 6 ms (67%), Space: 17.6 MB (98.35%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -2,7 +2,11 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         
         
-        if str(x)=="".join(list(reversed(str(x)))):
+        # if str(x)=="".join(list(reversed(str(x)))):
+        #     return True
+        # else:
+        #     return False
+        if str(x)==str(x)[::-1]:
             return True
         else:
             return False
